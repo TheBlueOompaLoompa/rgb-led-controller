3a0871f5cf636461c745eb79c0b8b758fdf20c58
Jeremy Nack <jnack@danfoss.com>
2020-12-05 18:27:51 -0600

    you would think I would learn

--- src/led.js
+++ src/led.js
@@ -9,8 +9,26 @@ let offset = 0;
 let lastTime = Date.now();
 let thisTime = Date.now();
 let nextStarBirthTime = 0;
-let starList = [{startTime:0, position:100, direction:1, speed:3, twinkleTime:1500, color:0xFF0000,},
-				{startTime:0, position:50, direction:-1, speed:5, twinkleTime:2000, color:0x00FF00,}];
+let starList = [{startTime:0, position:100, direction:1, speed:0.3, twinkleTime:1500, color:0xFF0000,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,},
+				{startTime:0, position:50, direction:-1, speed:0.5, twinkleTime:2000, color:0x00FF00,}];
 
 
 let animLoop = undefined;
