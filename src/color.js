c6472820bae2701eabf45824eb5fe2cd65c531d0
Benjamin Nack <ben@nackfamily.com>
2020-12-31 17:34:41 -0600

    Add simple color bank

---
+++ src/color.js
@@ -0,0 +1,9 @@
+module.exports = {
+    red: {r: 255, g: 0, b: 0},
+    orange: {r: 255, g: 90, b: 0},
+    yellow: {r: 255, g: 255, b: 0},
+    green: {r: 0, g: 255, b: 0},
+    blue: {r: 0, g: 0, b: 255},
+    purple: {r: 128, g: 0, b: 128},
+    pink: {r: 255, g: 192, b: 203},
+}
