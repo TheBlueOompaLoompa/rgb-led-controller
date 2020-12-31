71f0fcdbece62827674ecfc1a16ccfe86629d1c6
Benjamin Nack <ben@nackfamily.com>
2020-12-31 17:07:29 -0600

    Fix the spacing for a comment

--- led-control-app/frontend/src/patterns.js
+++ led-control-app/frontend/src/patterns.js
@@ -5,7 +5,7 @@ module.exports = {
 		} else if(grad == 1) {
 			return ['rgb(255, 90, 0)', 'white', 'yellow', 'white'];	// Thanksgiving
 		} else if(grad == 2) {
-			return ['green', 'rgb(252,113,217)', 'yellow', 'white'];			// Easter
+			return ['green', 'rgb(252,113,217)', 'yellow', 'white'];// Easter
 		} else if(grad == 3) {
 			return ['rgb(255, 90, 0)', 'green', 'purple', 'yellow'];// Halloween
 		} else if(grad == 4) {
