047a2ea73c999a116f4f3286f5bf6198f7163e68
Benjamin Nack <ben@nackfamily.com>
2020-12-31 22:32:33 -0600

    Fix asynchronus write

--- src/config.js
+++ src/config.js
@@ -8,8 +8,7 @@ module.exports = {
         return JSON.parse(fs.readFileSync(__dirname + '/../config.json', 'utf-8'));
     },
     write: (data) => {
-        console.log('save config');
-        fs.writeFile(__dirname + '/../config.json', JSON.stringify(data));
+        fs.writeFile(__dirname + '/../config.json', JSON.stringify(data), () => {console.log("Saved Config")});
     },
     writeSync: (data) => {
         fs.writeFileSync(__dirname + '/../config.json', JSON.stringify(data));
